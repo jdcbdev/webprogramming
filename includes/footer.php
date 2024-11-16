c88bff4cb3374f2c7c8b9fa92d094fb7969482ac
Jaydee Ballaho <jaydee.ballaho@wmsu.edu.ph>
2024-11-16 20:03:01 +0800

    separate the js

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,5 @@
 <script src="../vendor/jQuery-3.7.1/jquery-3.7.1.min.js"></script>
 <script src="../vendor/chartjs-4.4.5/chart.js"></script>
 <script src="../vendor/datatable-2.1.8/datatables.min.js"></script>
-<script src="../js/admin.js"></script>
 <script src="../js/product.js"></script>
+<script src="../js/admin.js"></script>
--- js/product.js
+++ js/product.js
@@ -1,34 +1,126 @@
-// Select all elements with the class 'deleteBtn'
-let deleteButtons = document.querySelectorAll('.deleteBtn');
+$(document).ready(function(){
+    window.viewProducts = function(){
+        $.ajax({
+            type: 'GET',
+            url: '../products/view-products.php',
+            dataType: 'html',
+            success: function(response){
+                $('.content-page').html(response)
 
-// Iterate over each button
-deleteButtons.forEach(button => {
-    // Add a 'click' event listener to each button
-    button.addEventListener('click', function(e) {
-        // Prevent the default action of the click event (e.g., navigation or form submission)
-        e.preventDefault();
-        
-        // Get the product name and id from the button's data attributes
-        let product = this.dataset.name;
-        let productID = this.dataset.id;
-        
-        // Ask the user for confirmation to delete the product
-        let response = confirm("Do you want to delete the product " + product + "?");
-        
-        // If the user confirms deletion
-        if (response) {
-            // Send a GET request to delete the product using the fetch API
-            fetch('../products/deleteproduct.php?id=' + productID, {
-                method: 'GET'
-            })
-            .then(response => response.text())  // Parse the response as plain text
-            .then(data => {
-                // If the server responds with 'success'
-                if(data === 'success') {
-                    // Redirect the user to 'product.php'
-                    window.location.href = 'admin/products';
+                var table = $('#table-products').DataTable({
+                    dom: 'rtp',
+                    pageLength: 10,
+                    ordering: false,
+                });
+
+                $('#custom-search').on('keyup', function() {
+                    table.search(this.value).draw()
+                });
+
+                $('#category-filter').on('change', function() {
+                    if(this.value !== 'choose'){
+                        table.column(3).search(this.value).draw()
+                    }
+                });
+
+                $('#add-product').on('click', function(e){
+                    e.preventDefault()
+                    addProduct()
+                })
+
+            }
+        })
+    }
+
+    function addProduct(){
+        $.ajax({
+            type: 'GET',
+            url: '../products/add-product.html',
+            dataType: 'html',
+            success: function(view){
+                $('.modal-container').html(view)
+                $('#modal-add-product').modal('show')
+
+                fetchCategories()
+
+                $('#form-add-product').on('submit', function(e){
+                    e.preventDefault()
+                    saveProduct()
+                })
+            }
+        })
+    }
+
+    function saveProduct(){
+        let form = new FormData($('#form-add-product')[0])
+        $.ajax({
+            type: 'POST',
+            url: '../products/add-product.php',
+            data: form,
+            dataType: 'json',
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                if (response.status === 'error') {
+                    if (response.codeErr) {
+                        $('#code').addClass('is-invalid');
+                        $('#code').next('.invalid-feedback').text(response.codeErr).show();
+                    }else{
+                        $('#code').removeClass('is-invalid');
+                    }
+                    if (response.nameErr) {
+                        $('#name').addClass('is-invalid');
+                        $('#name').next('.invalid-feedback').text(response.nameErr).show();
+                    }else{
+                        $('#name').removeClass('is-invalid');
+                    }
+                    if (response.categoryErr) {
+                        $('#category').addClass('is-invalid');
+                        $('#category').next('.invalid-feedback').text(response.categoryErr).show();
+                    }else{
+                        $('#category').removeClass('is-invalid');
+                    }
+                    if (response.priceErr) {
+                        $('#price').addClass('is-invalid');
+                        $('#price').next('.invalid-feedback').text(response.priceErr).show();
+                    }else{
+                        $('#price').removeClass('is-invalid');
+                    }
+                    if (response.imageErr) {
+                        $('#product_image').addClass('is-invalid');
+                        $('#product_image').next('.invalid-feedback').text(response.imageErr).show();
+                    }else{
+                        $('#product_image').removeClass('is-invalid');
+                    }
+                } else if (response.status === 'success') {
+                    $('#modal-add-product').modal('hide');
+                    $('#form-add-product')[0].reset();
+                    viewProducts()
                 }
-            });
-        }
-    });
-});
+            }
+        });
+        
+    }
+
+    function fetchCategories(){
+        $.ajax({
+            url: '../products/fetch-categories.php', // URL to the PHP script that returns the categories
+            type: 'GET',
+            dataType: 'json', // Expect JSON response
+            success: function(data) {
+                // Clear the existing options (if any) and add a default "Select" option
+                $('#category').empty().append('<option value="">--Select--</option>');
+                
+                // Iterate through the data (categories) and append each one to the select dropdown
+                $.each(data, function(index, category) {
+                    $('#category').append(
+                        $('<option>', {
+                            value: category.id, // The value attribute
+                            text: category.name // The displayed text
+                        })
+                    );
+                });
+            }
+        });
+    }
+})
